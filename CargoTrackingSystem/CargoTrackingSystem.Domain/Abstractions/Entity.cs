dc9357b60d093da4a02b755cc185d891a2251cb0
Sila Kuscu <silaksc27@gmail.com>
2025-09-24 17:12:03 +0300

    [create] base class added

---
+++ CargoTrackingSystem/CargoTrackingSystem.Domain/Abstractions/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace CargoTrackingSystem.Domain.Abstractions;
+
+public abstract class Entity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+}
