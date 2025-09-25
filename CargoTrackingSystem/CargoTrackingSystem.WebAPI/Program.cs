73cca1e2da7bbbdd3396d4a01a5b7286058fc660
Sila Kuscu <silaksc27@gmail.com>
2025-09-25 13:09:05 +0300

    [add] Add API startup configuration

---
+++ CargoTrackingSystem/CargoTrackingSystem.WebAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using CargoTrackingSystem.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); // add for identity
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
