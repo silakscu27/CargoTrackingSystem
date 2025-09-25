e102fd80b7395bd3f6079e13a786ab99f8df0273
Sila Kuscu <silaksc27@gmail.com>
2025-09-25 13:08:23 +0300

    [add] Add dependency injection configuration

---
+++ CargoTrackingSystem/CargoTrackingSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,64 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using CargoTrackingSystem.Infrastructure.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using System.Reflection;
+
+namespace CargoTrackingSystem.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // UnitOfWork
+            services.AddScoped<IUnitOfWork>(srv => srv.GetRequiredService<ApplicationDbContext>());
+
+            // Identity
+            services.AddIdentity<AppUser, IdentityRole<Guid>>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireDigit = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            // JWT Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer();
+
+            services.AddAuthorization();
+
+            // Auto register Repositories / Services (Scrutor)
+            services.Scan(scan => scan
+                .FromAssemblies(Assembly.GetExecutingAssembly())
+                .AddClasses()
+                .AsMatchingInterface()
+                .WithScopedLifetime()
+            );
+
+            // Health Checks
+            services.AddHealthChecks()
+                .AddDbContextCheck<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
