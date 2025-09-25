c52f977cd89954c8001f1365e137f84fc984af26
Sila Kuscu <silaksc27@gmail.com>
2025-09-25 13:10:06 +0300

    [add] Add database context class

---
+++ CargoTrackingSystem/CargoTrackingSystem.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿using CargoTrackingSystem.Domain.Abstractions;
+using CargoTrackingSystem.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CargoTrackingSystem.Infrastructure.Context
+{
+    internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Entity DbSets
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Shipment> Shipments { get; set; }
+        public DbSet<ShipmentStatusHistory> ShipmentStatusHistories { get; set; }
+        public DbSet<ShipmentTransfer> ShipmentTransfers { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Fluent API configuration
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            // Ignore some fields in identity tables
+            builder.Ignore<IdentityUserLogin<Guid>>();
+            builder.Ignore<IdentityRoleClaim<Guid>>();
+            builder.Ignore<IdentityUserToken<Guid>>();
+            builder.Ignore<IdentityUserRole<Guid>>();
+            builder.Ignore<IdentityUserClaim<Guid>>();
+
+            base.OnModelCreating(builder);
+        }
+
+        // IUnitOfWork implementation
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
