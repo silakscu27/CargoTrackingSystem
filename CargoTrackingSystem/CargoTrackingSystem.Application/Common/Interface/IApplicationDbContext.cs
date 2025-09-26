11c960a119d1a77c01bd6f181ec8f2671d554d61
Sila Kuscu <silaksc27@gmail.com>
2025-09-26 16:10:26 +0300

    [add] Add ApplicationDbContext interface

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Common/Interface/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CargoTrackingSystem.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Shipment> Shipments { get; set; }
+    public DbSet<ShipmentStatusHistory> ShipmentStatusHistories { get; set; }
+    public DbSet<Warehouse> Warehouses { get; set; }
+    public DbSet<ShipmentTransfer> ShipmentTransfers { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    int SaveChanges();
+}
+ 
