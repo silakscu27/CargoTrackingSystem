bae3dade5734bf99aa089b721d916fa38000616c
Sila Kuscu <silaksc27@gmail.com>
2025-09-29 17:25:11 +0300

    [add] Add Customer get all CQRS query and handler

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/Customers/Queries/GetAll/CustomerGetAllQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using CargoTrackingSystem.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.Customers.Queries.GetAll;
+
+internal sealed class CustomerGetAllQueryHandler(
+    ICustomerRepository customerRepository) : IRequestHandler<CustomerGetAllQuery, Result<List<Customer>>>
+{
+    public async Task<Result<List<Customer>>> Handle(CustomerGetAllQuery request, CancellationToken cancellationToken)
+    {
+        List<Customer> customers = await customerRepository.GetAllAsync(cancellationToken);
+
+        return customers.Any()
+            ? Result<List<Customer>>.Succeed(customers)
+            : Result<List<Customer>>.Failure("Müşteri bulunamadı");
+    }
+}
