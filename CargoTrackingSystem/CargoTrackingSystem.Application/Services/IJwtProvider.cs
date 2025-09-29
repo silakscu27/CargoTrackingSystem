43ecd4b7837ad351a68f127a91828a525105e12d
Sila Kuscu <silaksc27@gmail.com>
2025-09-29 14:41:54 +0300

    [add] Add JWT service interface

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Services/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using CargoTrackingSystem.Application.Features.Auth.Login;
+using CargoTrackingSystem.Domain.Entities;
+
+namespace CargoTrackingSystem.Application.Services;
+
+public interface IJwtProvider
+{
+    Task<LoginCommandResponse> CreateToken(AppUser user);
+}
