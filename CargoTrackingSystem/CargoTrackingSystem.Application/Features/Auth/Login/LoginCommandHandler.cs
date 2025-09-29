b20b0743d4a9ed2a2e6028e913834b1fd90c906d
Sila Kuscu <silaksc27@gmail.com>
2025-09-29 15:46:01 +0300

    [add] Add login CQRS command and handler

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/Auth/Login/LoginCommandHandler.cs
@@ -0,0 +1,62 @@
+﻿using CargoTrackingSystem.Application.Services;
+using CargoTrackingSystem.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.Auth.Login;
+
+internal sealed class LoginCommandHandler(
+    UserManager<AppUser> userManager,
+    IJwtProvider jwtProvider) : IRequestHandler<LoginCommand, Result<LoginCommandResponse>>
+{
+    public async Task<Result<LoginCommandResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        AppUser? user = await userManager.Users
+            .FirstOrDefaultAsync(p =>
+                p.UserName == request.EmailOrUserName ||
+                p.Email == request.EmailOrUserName,
+                cancellationToken);
+
+        if (user is null)
+        {
+            return Result<LoginCommandResponse>.Failure("Kullanıcı bulunamadı");
+        }
+
+        // Check if user is locked
+        if (await userManager.IsLockedOutAsync(user))
+        {
+            var lockoutEnd = await userManager.GetLockoutEndDateAsync(user);
+            if (lockoutEnd.HasValue)
+            {
+                var timeRemaining = lockoutEnd.Value - DateTime.UtcNow;
+                return Result<LoginCommandResponse>.Failure($"Şifrenizi 3 defa yanlış girdiğiniz için kullanıcı {Math.Ceiling(timeRemaining.TotalMinutes)} dakika süreyle bloke edilmiştir");
+            }
+            return Result<LoginCommandResponse>.Failure("Kullanıcınız 3 kez yanlış şifre girdiği için 5 dakika süreyle bloke edilmiştir");
+        }
+
+        // Check if your email is verified
+        if (!await userManager.IsEmailConfirmedAsync(user))
+        {
+            return Result<LoginCommandResponse>.Failure("Mail adresiniz onaylı değil");
+        }
+
+        // Password controll
+        bool isPasswordCorrect = await userManager.CheckPasswordAsync(user, request.Password);
+
+        if (!isPasswordCorrect)
+        {
+            // Save the incorrect entry
+            await userManager.AccessFailedAsync(user);
+            return Result<LoginCommandResponse>.Failure("Şifreniz yanlış");
+        }
+
+        // Successful login - reset error counter
+        await userManager.ResetAccessFailedCountAsync(user);
+
+        // Create token
+        var loginResponse = await jwtProvider.CreateToken(user);
+        return Result<LoginCommandResponse>.Succeed(loginResponse);
+    }
+}
---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/Auth/Login/LoginCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace CargoTrackingSystem.Application.Features.Auth.Login;
+
+public sealed class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(p => p.EmailOrUserName)
+            .MinimumLength(3)
+            .WithMessage("Kullanıcı adı ya da mail bilgisi en az 3 karakter olmalıdır");
+
+        RuleFor(p => p.Password)
+            .MinimumLength(4)
+            .WithMessage("Şifre en az 4 karakter olmalıdır");
+    }
+}
