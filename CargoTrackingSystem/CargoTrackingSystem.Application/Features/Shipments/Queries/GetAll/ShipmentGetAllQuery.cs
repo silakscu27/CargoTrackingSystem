fce9bb721c324f6412cb880f0fa7cea238f1b312
Sila Kuscu <silaksc27@gmail.com>
2025-09-30 17:12:18 +0300

    [add] Add Shipment get all CQRS query and handler

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/Shipments/Queries/GetAll/ShipmentGetAllQuery.cs
@@ -0,0 +1,7 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.Shipments.Queries.GetAll;
+
+public sealed record ShipmentGetAllQuery() : IRequest<Result<List<Shipment>>>;
---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/Shipments/Queries/GetAll/ShipmentGetAllQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using CargoTrackingSystem.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.Shipments.Queries.GetAll;
+
+internal sealed class ShipmentGetAllQueryHandler(
+    IShipmentRepository shipmentRepository) : IRequestHandler<ShipmentGetAllQuery, Result<List<Shipment>>>
+{
+    public async Task<Result<List<Shipment>>> Handle(ShipmentGetAllQuery request, CancellationToken cancellationToken)
+    {
+        List<Shipment> shipments = await shipmentRepository.GetAllAsync(cancellationToken);
+
+        return shipments.Any()
+            ? Result<List<Shipment>>.Succeed(shipments)
+            : Result<List<Shipment>>.Failure("Kargo bulunamadı");
+    }
+}
