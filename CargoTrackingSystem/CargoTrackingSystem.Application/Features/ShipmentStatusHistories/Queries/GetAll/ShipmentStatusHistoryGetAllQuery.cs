e77eab1697cd321211413da9501e5559d50b75e7
Sila Kuscu <silaksc27@gmail.com>
2025-09-30 17:30:50 +0300

    [add] Add ShipmentStatusHistory get all CQRS query and handler

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/ShipmentStatusHistories/Queries/GetAll/ShipmentStatusHistoryGetAllQuery.cs
@@ -0,0 +1,7 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.ShipmentStatusHistories.Queries.GetAll;
+
+public sealed record ShipmentStatusHistoryGetAllQuery() : IRequest<Result<List<ShipmentStatusHistory>>>;
---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/ShipmentStatusHistories/Queries/GetAll/ShipmentStatusHistoryGetAllQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using CargoTrackingSystem.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.ShipmentStatusHistories.Queries.GetAll;
+
+internal sealed class ShipmentStatusHistoryGetAllQueryHandler(
+    IShipmentStatusHistoryRepository repository
+) : IRequestHandler<ShipmentStatusHistoryGetAllQuery, Result<List<ShipmentStatusHistory>>>
+{
+    public async Task<Result<List<ShipmentStatusHistory>>> Handle(
+        ShipmentStatusHistoryGetAllQuery request,
+        CancellationToken cancellationToken)
+    {
+        var histories = await repository.GetAllAsync(cancellationToken);
+
+        return histories.Any()
+            ? Result<List<ShipmentStatusHistory>>.Succeed(histories)
+            : Result<List<ShipmentStatusHistory>>.Failure("Hiçbir kargo durumu bulunamadı");
+    }
+}
