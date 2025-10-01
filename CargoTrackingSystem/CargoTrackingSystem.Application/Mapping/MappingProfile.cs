de2b3f0ef58d6585dc04781d55b4662d3f36752e
Sila Kuscu <silaksc27@gmail.com>
2025-10-01 10:41:03 +0300

    [add] Add AutoMapper profile

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CargoTrackingSystem.Application.Features.Customers.Commands.Add;
+using CargoTrackingSystem.Application.Features.Customers.CustomerUpdate;
+using CargoTrackingSystem.Application.Features.Customers.Queries.GetAll;
+using CargoTrackingSystem.Application.Features.Customers.Queries.GetById;
+using CargoTrackingSystem.Application.Features.Shipments.Commands.Add;
+using CargoTrackingSystem.Application.Features.Shipments.Commands.Update;
+using CargoTrackingSystem.Application.Features.Shipments.Queries.GetById;
+using CargoTrackingSystem.Domain.Entities;
+
+namespace CargoTrackingSystem.Application.Mapping
+{
+    public sealed class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Customer mappings
+            CreateMap<CustomerAddCommand, Customer>();
+            CreateMap<CustomerUpdateCommand, Customer>();
+            CreateMap<Customer, CustomerGetByIdDto>();
+
+            // Shipment mappings
+            CreateMap<ShipmentAddCommand, Shipment>();
+            CreateMap<ShipmentUpdateCommand, Shipment>();
+            CreateMap<Shipment, ShipmentGetByIdDto>();
+        }
+    }
+}
