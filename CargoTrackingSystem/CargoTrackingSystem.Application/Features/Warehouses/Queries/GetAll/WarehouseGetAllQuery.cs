bf0173698a2d8ab000f765798a645d91c3ce6c5e
Sila Kuscu <silaksc27@gmail.com>
2025-10-01 16:28:26 +0300

    [add] Add Warehouse get all CQRS query and handler

---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/Warehouses/Queries/GetAll/WarehouseGetAllQuery.cs
@@ -0,0 +1,7 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.Warehouses.Queries.GetAll;
+
+public sealed record WarehouseGetAllQuery() : IRequest<Result<List<Warehouse>>>;
---
+++ CargoTrackingSystem/CargoTrackingSystem.Application/Features/Warehouses/Queries/GetAll/WarehouseGetAllQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using CargoTrackingSystem.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace CargoTrackingSystem.Application.Features.Warehouses.Queries.GetAll;
+
+internal sealed class WarehouseGetAllQueryHandler(
+    IWarehouseRepository warehouseRepository
+) : IRequestHandler<WarehouseGetAllQuery, Result<List<Warehouse>>>
+{
+    public async Task<Result<List<Warehouse>>> Handle(WarehouseGetAllQuery request, CancellationToken cancellationToken)
+    {
+        List<Warehouse> warehouses = await warehouseRepository.GetAllAsync(cancellationToken);
+
+        return warehouses.Any()
+            ? Result<List<Warehouse>>.Succeed(warehouses)
+            : Result<List<Warehouse>>.Failure("Depo bulunamadı");
+    }
+}
