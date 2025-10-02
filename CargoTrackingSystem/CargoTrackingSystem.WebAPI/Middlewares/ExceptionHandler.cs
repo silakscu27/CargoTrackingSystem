532cfb4fe32ae8646897d0e205abe685f8775107
Sila Kuscu <silaksc27@gmail.com>
2025-10-02 14:58:33 +0300

    [add] Add exception handler and middleware

---
+++ CargoTrackingSystem/CargoTrackingSystem.WebAPI/Middlewares/ExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using TS.Result;
+
+namespace CargoTrackingSystem.WebAPI.Middlewares
+{
+    public class ExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            Result<string> errorResult;
+
+            httpContext.Response.ContentType = "application/json";
+
+            // If there is a validation error return 403.
+            if (exception is ValidationException validationException)
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+
+                var errorFields = validationException.Errors.Select(e => e.PropertyName).ToList();
+
+                errorResult = Result<string>.Failure(StatusCodes.Status403Forbidden, errorFields);
+
+                await httpContext.Response.WriteAsync(JsonSerializer.Serialize(errorResult), cancellationToken);
+                return true;
+            }
+
+            // other errors 500
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            errorResult = Result<string>.Failure(exception.Message);
+
+            await httpContext.Response.WriteAsync(JsonSerializer.Serialize(errorResult), cancellationToken);
+
+            return true;
+        }
+    }
+}
---
+++ CargoTrackingSystem/CargoTrackingSystem.WebAPI/Middlewares/ExtensionsMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using CargoTrackingSystem.Domain.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CargoTrackingSystem.WebAPI.Middlewares
+{
+    public static class ExtensionsMiddleware
+    {
+        public static void CreateFirstUser(WebApplication app)
+        {
+            using (var scoped = app.Services.CreateScope())
+            {
+                var userManager = scoped.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+                if (!userManager.Users.Any(p => p.UserName == "admin"))
+                {
+                    AppUser user = new()
+                    {
+                        UserName = "admin",
+                        Email = "admin@admin.com",
+                        FirstName = "Sıla",
+                        LastName = "Kuscu",
+                        EmailConfirmed = true
+                    };
+
+                    // Default password
+                    userManager.CreateAsync(user, "admin123").Wait();
+                }
+            }
+        }
+    }
+}
