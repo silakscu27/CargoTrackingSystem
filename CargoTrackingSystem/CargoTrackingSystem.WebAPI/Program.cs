ec1b9f9741979b766efae1deef9d02511e237834
Sila Kuscu <silaksc27@gmail.com>
2025-10-03 17:32:53 +0300

    [update] Update Program.cs startup configuration

--- CargoTrackingSystem/CargoTrackingSystem.WebAPI/Program.cs
+++ CargoTrackingSystem/CargoTrackingSystem.WebAPI/Program.cs
@@ -1,18 +1,35 @@
 ﻿using CargoTrackingSystem.Infrastructure;
+using CargoTrackingSystem.WebAPI.Middlewares;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblies(
+        Assembly.GetExecutingAssembly(),
+        typeof(CargoTrackingSystem.Application.Features.Auth.Login.LoginCommand).Assembly
+    );
+});
+
+// builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+ExtensionsMiddleware.CreateFirstUser(app);
+
+app.UseMiddleware<ExceptionHandler>();
 
 if (app.Environment.IsDevelopment())
 {
@@ -21,10 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication(); // add for identity
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
