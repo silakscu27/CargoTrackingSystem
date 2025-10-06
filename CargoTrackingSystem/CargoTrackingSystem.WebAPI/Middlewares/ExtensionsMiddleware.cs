ffc6ecdde9166ce4adbc8279a9388e0ba3750abe
Sila Kuscu <silaksc27@gmail.com>
2025-10-06 10:50:26 +0300

    [add] Add 2 new users to middleware

--- CargoTrackingSystem/CargoTrackingSystem.WebAPI/Middlewares/ExtensionsMiddleware.cs
+++ CargoTrackingSystem/CargoTrackingSystem.WebAPI/Middlewares/ExtensionsMiddleware.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace CargoTrackingSystem.WebAPI.Middlewares
 {
@@ -13,9 +14,10 @@ namespace CargoTrackingSystem.WebAPI.Middlewares
             {
                 var userManager = scoped.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
 
+                // 1️- Admin User
                 if (!userManager.Users.Any(p => p.UserName == "admin"))
                 {
-                    AppUser user = new()
+                    AppUser admin = new()
                     {
                         UserName = "admin",
                         Email = "admin@admin.com",
@@ -24,8 +26,37 @@ namespace CargoTrackingSystem.WebAPI.Middlewares
                         EmailConfirmed = true
                     };
 
-                    // Default password
-                    userManager.CreateAsync(user, "admin123").Wait();
+                    userManager.CreateAsync(admin, "admin123").Wait();
+                }
+
+                // 2️- Manager User
+                if (!userManager.Users.Any(p => p.UserName == "manager"))
+                {
+                    AppUser manager = new()
+                    {
+                        UserName = "manager",
+                        Email = "manager@cargo.com",
+                        FirstName = "Ahmet",
+                        LastName = "Yılmaz",
+                        EmailConfirmed = true
+                    };
+
+                    userManager.CreateAsync(manager, "manager123").Wait();
+                }
+
+                // 3️- Customer User
+                if (!userManager.Users.Any(p => p.UserName == "customer"))
+                {
+                    AppUser customer = new()
+                    {
+                        UserName = "customer",
+                        Email = "customer@cargo.com",
+                        FirstName = "Zeynep",
+                        LastName = "Demir",
+                        EmailConfirmed = true
+                    };
+
+                    userManager.CreateAsync(customer, "customer123").Wait();
                 }
             }
         }
